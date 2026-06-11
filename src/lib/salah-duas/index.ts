299edaa006c04e057a71766ea018d05135ef5760
Shihab Mahamud <shihab4t@gmail.com>
2026-06-11 15:55:17 +0600

    wip

--- src/lib/salah-duas/index.ts
+++ src/lib/salah-duas/index.ts
@@ -3,6 +3,11 @@ import type { TSalahDua } from '../../types/salah-duas'
 
 import { beforeTasleemDuas } from './before-tasleem'
 import { jalsahDuas } from './jalsah'
+import { istiftah1 } from './opening-duas/istiftah-1'
+import { istiftah2 } from './opening-duas/istiftah-2'
+import { istiftah3 } from './opening-duas/istiftah-3'
+import { istiftahTahajjud } from './opening-duas/istiftah-tahajjud'
+import { takbeer } from './opening-duas/takbeer'
 import { qawmahDuas } from './qawmah'
 import { rukuDuas } from './ruku'
 import { sajdahDuas } from './sajdah'
@@ -11,6 +16,11 @@ import { taawwudhDuas } from './taawwudh'
 import { tashahhudDuas } from './tashahhud'
 
 export const allDuas: TSalahDua[] = [
+    takbeer,
+    istiftah1,
+    istiftah2,
+    istiftah3,
+    istiftahTahajjud,
     ...taawwudhDuas,
     ...rukuDuas,
     ...qawmahDuas,

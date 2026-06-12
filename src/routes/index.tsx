fa5aa6549df454eb388209351bb51deddae6ce39
Shihab Mahamud <shihab4t@gmail.com>
2026-06-12 16:45:29 +0600

    wip

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,13 @@ import type { TSalahPositionId } from '#/types/salah-positions'
 
 import { createFileRoute } from '@tanstack/react-router'
 
-import { SALAH_POSITIONS } from '#/lib/salah-positions'
-
 import { DuasBrowser } from '#/components/screens/duas-browser'
 import { ClosingNote } from '#/components/screens/home/closing-note'
 import { Features } from '#/components/screens/home/features'
 import { Hero } from '#/components/screens/home/hero'
 import { Separator } from '#/components/ui/separator'
+
+import { SALAH_POSITIONS } from '#/lib/salah-positions'
 
 const positionIds: string[] = SALAH_POSITIONS.map((p) => p.id)
 
